c7cc60267d8c90d255e90a8fd904c9ca90c84f36
kjw1109 <kjw1109@midasit.com>
2024-12-26 22:42:41 +0900

    style: skill 섹션 수정

--- src/components/sections/Skill/style.ts
+++ src/components/sections/Skill/style.ts
@@ -6,14 +6,14 @@ export const SkillContainer = styled.section`
 `;
 
 export const SkillName = styled.h2`
-  font-size: 1.5vw;
+  font-size: 2vw;
   font-weight: 500;
   line-height: 1;
   letter-spacing: -0.01em;
 `;
 export const SkillStacks = styled.h3`
   margin: 0.5vw 0 1.5vw;
-  font-size: 3vw;
+  font-size: 5vw;
   font-weight: 600;
   letter-spacing: -0.06em;
 `;
@@ -21,7 +21,7 @@ export const SkillStacks = styled.h3`
 export const SkillDescription = styled.p`
   width: 50%;
   margin: 0 auto;
-  font-size: 1vw;
+  font-size: 1.3vw;
   line-height: 1.5;
   word-break: keep-all;
 `;
@@ -33,7 +33,7 @@ export const StyledSwiper = styled(Swiper)`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    width: 50vw;
+    width: 75vw;
     aspect-ratio: 16/9;
     background: white;
     text-align: center;
